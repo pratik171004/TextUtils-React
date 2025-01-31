69ed8697c2acdb82a0fabcf8d62547e6c0f973f0
Pratik <pratikwaghmare546@gmail.com>
2025-01-31 23:48:29 +0530

    Creating react router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React, {useState} from 'react'
 import './App.css';
 import Navbar from './components/Navbar';
-//import About from './components/About';
+import About from './components/About';
 import TextForm from './components/TextForm';
 import Alert from './components/Alert';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom"
 
 function App() {
   const [mode,setMode]=useState('light');
@@ -25,23 +31,33 @@ function App() {
     setMode('dark');
     document.body.style.backgroundColor='#042743';
     showAlert("Dark mode has been enabled","success");
+    document.title="TextUtils-Dark Mode";
   }
   else{
    setMode('light');
    document.body.style.backgroundColor='white';
    showAlert("Light mode has been enabled","success");
+   document.title="TextUtils-Light Mode";
   }
 }
   return (
 <>
+<Router>
 <Navbar title='TextUtils' mode={mode} toggleMode={toggleMode} />
 <Alert alert={alert}/>
 <br />
 <div className="container">
-   <TextForm  showAlert={showAlert} heading="Enter the text to analyze below" mode={mode}/> 
-  {/* <About/> */}
+<Switch>
+          <Route exact path="/about">      //Use exact keyword to exact matching
+            <About />
+          </Route>
+          <Route exact path="/">
+              <TextForm  showAlert={showAlert} heading="Enter the text to analyze below" mode={mode}/> 
+          </Route>
+        </Switch>
 
 </div>
+</Router>
 </>
   );
 }
